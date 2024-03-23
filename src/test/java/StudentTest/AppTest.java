6d093fac14050a3c582088c7c9d4d9eb6b9d2932
David Horvath <89983087+913-Horvath-David@users.noreply.github.com>
2024-03-23 14:49:58 +0200

    Update AppTest.java

--- src/test/java/StudentTest/AppTest.java
+++ src/test/java/StudentTest/AppTest.java
@@ -21,7 +21,7 @@ public class AppTest {
     public void testAddStudent2() {
         // This should fail
         StudentRepository studentRepository = new StudentRepository(new StudentValidator());
-        Student student = new Student("1", "Andrei", 105);
+        Student student = new Student("1", "Andrei", 936);
         studentRepository.save(student);
         assertEquals(studentRepository.findOne("1"), student);
     }
