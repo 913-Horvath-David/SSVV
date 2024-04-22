c0ff8c6042e8dc7410a6c65b58e3a64844a5b7c1
Horvath David <89983087+913-Horvath-David@users.noreply.github.com>
2024-04-22 17:55:48 +0300

    Fix test not running

--- src/test/java/GradeTest/AppTest.java
+++ src/test/java/GradeTest/AppTest.java
@@ -3,9 +3,7 @@ package GradeTest;
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
@@ -15,50 +13,80 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.Validator;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    private static Service studentService;
-    private static Service temaService;
+    private Service service;
+    private StudentXMLRepository studentXMLRepository;
+    private TemaXMLRepository temaXMLRepository;
+    private NotaXMLRepository notaXMLRepository;
 
-    private static Service notaService;
+    private static final String STUDENTI_FILE = "studenti_test.xml";
+    private static final String TEME_FILE = "teme_test.xml";
+    private static final String NOTE_FILE = "note_test.xml";
 
-    @BeforeAll
-    public static void setup(){
-        Validator<Student> studentValidator = new StudentValidator();
-        StudentXMLRepository studentRepository = new StudentXMLRepository(studentValidator, "test_studenti.xml");
-        studentService = new Service(studentRepository, null, null);
+    @BeforeEach
+    protected void setUp() {
+        createTestXMLFile(STUDENTI_FILE);
+        createTestXMLFile(TEME_FILE);
+        createTestXMLFile(NOTE_FILE);
 
-        Validator<Tema> temaValidator = new TemaValidator();
-        TemaXMLRepository temaRepository = new TemaXMLRepository(temaValidator, "test_teme.xml");
-        temaService = new Service(null, temaRepository, null);
+        this.studentXMLRepository = new StudentXMLRepository(new StudentValidator(), STUDENTI_FILE);
+        this.temaXMLRepository = new TemaXMLRepository(new TemaValidator(), TEME_FILE);
+        this.notaXMLRepository = new NotaXMLRepository(new NotaValidator(), NOTE_FILE);
+        this.service = new Service(this.studentXMLRepository, this.temaXMLRepository, this.notaXMLRepository);
+    }
 
-        Validator<Nota> notaValidator = new NotaValidator();
-        NotaXMLRepository notaRepository = new NotaXMLRepository(notaValidator, "test_note.xml");
-        notaService = new Service(studentRepository, temaRepository, notaRepository);
+    @AfterEach
+    protected void removeTestFiles() {
+        new File(STUDENTI_FILE).delete();
+        new File(TEME_FILE).delete();
+        new File(NOTE_FILE).delete();
+    }
+
+    private void createTestXMLFile(String fileName) {
+        File xml = new File(fileName);
+        String xmlContent = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<inbox>\n</inbox>";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(xml))) {
+            writer.write(xmlContent);
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println("Failed to create XML file: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void addStudent_WithValidData_ShouldAddSuccessfully() {
-        int initialCount = studentService.countStudents();
-        assertEquals(1, studentService.saveStudent("500", "Alex", 933));
-        assertEquals(initialCount + 1, studentService.countStudents());
+        int initialCount = service.countStudents();
+        assertEquals(1, service.saveStudent("500", "Alex", 933));
+        assertEquals(initialCount + 1, service.countStudents());
     }
 
     @Test
     public void addTema_WithValidData_ShouldAddSuccessfully() {
-        assertEquals(1, temaService.saveTema("510", "tema", 5, 1));
+        assertEquals(1, service.saveTema("510", "tema", 5, 1));
     }
 
-    @AfterAll
-    public static void addNota_WithValidData_ShouldAddSuccessfully() {
-        assertEquals(1, notaService.saveNota("500", "510", 7, 3, "Good"));
+    @Test
+    public void addNota_WithValidData_ShouldAddSuccessfully() {
+        int initialCount = service.countStudents();
+        assertEquals(1, service.saveStudent("1", "Mihai", 936));
+        assertEquals(initialCount + 1, service.countStudents());
+        assertEquals(1, service.saveTema("2", "Tema SSVV", 5, 1));
+        assertEquals(1, service.saveNota("1", "2", 10, 8, "Great job!"));
     }
 
     @Test
     public void addGrade_integration() {
-        assertEquals(1, studentService.saveStudent("999", "Maria", 933));
-        assertEquals(1, temaService.saveTema("222", "tema", 10, 1));
-        assertEquals(1, notaService.saveNota("999", "222", 10, 8, "Great job!"));
+        assertEquals(1, service.saveStudent("999", "Maria", 933));
+        assertEquals(1, service.saveTema("222", "tema", 10, 1));
+        assertEquals(1, service.saveNota("999", "222", 10, 8, "Great job!"));
     }
 }
